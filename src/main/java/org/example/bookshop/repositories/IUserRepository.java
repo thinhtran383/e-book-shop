30dcfc9eb762fe2a6408aaa71ebda08e039e88fa
ThinhTran <thinhtran3803@gmail.com>
2024-09-22 21:14:26 +0700

    implement authentication

--- src/main/java/org/example/bookshop/repositories/IUserRepository.java
+++ src/main/java/org/example/bookshop/repositories/IUserRepository.java
@@ -3,5 +3,8 @@ package org.example.bookshop.repositories;
 import org.example.bookshop.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface IUserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByUsername(String username);
 }
