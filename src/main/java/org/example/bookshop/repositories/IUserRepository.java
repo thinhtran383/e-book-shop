0718613adf89fc8d9315f38e8a566423504f7658
ThinhTran <thinhtran3803@gmail.com>
2024-09-23 21:17:48 +0700

    refactor to text block

--- src/main/java/org/example/bookshop/repositories/IUserRepository.java
+++ src/main/java/org/example/bookshop/repositories/IUserRepository.java
@@ -1,5 +1,6 @@
 package org.example.bookshop.repositories;
 
+import org.example.bookshop.dto.ExistUserDto;
 import org.example.bookshop.entities.User;
 import org.example.bookshop.responses.users.LoginResponse;
 import org.example.bookshop.responses.users.UserResponse;
@@ -16,10 +17,22 @@ import java.util.Optional;
 public interface IUserRepository extends JpaRepository<User, Integer> {
     Optional<User> findByUsername(String username);
 
-    @Query("SELECT new org.example.bookshop.responses.users.UserResponse(u.id, u.username, u.email, c.fullName, c.phone, c.address) " +
-            "FROM User u JOIN u.customer c JOIN u.role r " +
-            "WHERE r.roleName = 'customer'")
+    @Query("""
+            SELECT new org.example.bookshop.responses.users.UserResponse(u.id, u.username, u.email, c.fullName, c.phone, c.address)
+            FROM User u 
+            JOIN u.customer c 
+            JOIN u.role r 
+            WHERE r.roleName = 'customer'
+            """)
     Page<UserResponse> findAllUserDetails(Pageable pageable);
+
+    @Query("""
+            SELECT COUNT(u) > 0
+            FROM User u
+            JOIN u.customer c
+            WHERE u.email = :email OR c.phone = :phone
+            """)
+    boolean existsByEmailOrPhone(@Param("email") String email, @Param("phone") String phone);
 
 
 //    @Query("select new org.example.bookshop.responses.users.LoginResponse(u.username, c.fullName, u.email, c.address, c.phone, r.roleName)" +
