3029eb54d6fe0e61f51da0ab1487fd98c1148c1d
ThinhTran <thinhtran3803@gmail.com>
2024-09-25 20:38:02 +0700

    update swagger authorization bearer token

--- src/main/java/org/example/bookshop/config/SwaggerConfig.java
+++ src/main/java/org/example/bookshop/config/SwaggerConfig.java
@@ -2,14 +2,34 @@ package org.example.bookshop.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.servers.Server;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+import io.swagger.v3.oas.models.security.SecurityRequirement;
 
 @OpenAPIDefinition(
         servers = {
-                @Server(url = "https://api.thinhtran.online", description = "Server URL for production"),
+                @Server(url = "http://localhost:8080", description = "Server URL for development"),
+                @Server(url = "https://api.thinhtran.online", description = "Server URL for production")
+
         }
 )
 @Configuration
 public class SwaggerConfig {
+        private SecurityScheme createAPIKeyScheme() {
+                return new SecurityScheme().type(SecurityScheme.Type.HTTP)
+                        .bearerFormat("JWT")
+                        .scheme("bearer");
+        }
+
+        @Bean
+        public OpenAPI openAPI() {
+                return new OpenAPI().addSecurityItem(new SecurityRequirement().
+                                addList("Bearer Authentication"))
+                        .components(new Components().addSecuritySchemes
+                                ("Bearer Authentication", createAPIKeyScheme()));
+        }
 }
+
