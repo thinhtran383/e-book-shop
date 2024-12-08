cb87beb76f3f5226cb7b56221763f68df084fcab
ThinhTran <thinhtran3803@gmail.com>
2024-12-08 14:40:55 +0700

    test

--- src/main/java/org/example/bookshop/controllers/HomeController.java
+++ src/main/java/org/example/bookshop/controllers/HomeController.java
@@ -1,14 +1,13 @@
 package org.example.bookshop.controllers;
 
 import lombok.RequiredArgsConstructor;
-import org.example.bookshop.dto.mail.MailDto;
 import org.example.bookshop.responses.PageableResponse;
 import org.example.bookshop.responses.Response;
 import org.example.bookshop.services.MailService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import reactor.core.publisher.Mono;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.security.Principal;
 import java.util.List;
@@ -38,6 +37,7 @@ public class HomeController {
     public ResponseEntity<Response> userCredentials(Principal principal) {
         return ResponseEntity.ok(Response.builder()
                 .message(principal.getName())
+
                 .build());
     }
 
